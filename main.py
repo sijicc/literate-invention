43abef89fff67a656c006c4187ef84a8e915051b
sijicc <54640861+sijicc@users.noreply.github.com>
2021-01-04 22:47:54 +0100

    added some comments

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from natsort import natsorted
 from os import getcwd
 
 sg.theme('default')
-cwd = getcwd()
+cwd = getcwd() #get current working directory
 
-text_column = [
+text_column = [ #text column
     [sg.Text('Pick directory of whoever you want to check,'
              '\nthen click on "Merge" and wait for a bit.'
              '\nAfter you see "Merged!" in output area'
@@ -19,7 +19,7 @@ text_column = [
              '\nElse it will just crash.', text_color="red")]
 ]
 
-input_column = [
+input_column = [ #input column
     [sg.Input(cwd, size=(25, 1), key="-DIRECTORY-"), sg.FolderBrowse()],
     [sg.Button('Merge'), sg.Button('Fix encoding')],
     [sg.InputText(key="-WORD-", size=(25, 1)), sg.Button('Count')],
@@ -27,7 +27,7 @@ input_column = [
     [sg.Button('Exit')]
 ]
 
-result_column = [
+result_column = [ #output/result column
     [
         sg.Output(size=(30, 10))
      ]
@@ -56,12 +56,12 @@ while True:
     if event == 'Merge':
         output_file = []  # declare empty list to extend
         print(path)
-        for file_name in natsorted(glob.glob(path + "/message_*.json")):
-            print(file_name)
+        for file_name in natsorted(glob.glob(path + "/message_*.json")): # sorts naturally from 1 to x
+            print(file_name) 
             with open(file_name) as json_file:
                 messages = json.load(json_file)
             json_messages = messages["messages"]
-            output_file.extend(json_messages)
+            output_file.extend(json_messages) #extends output_file with message_1, 2.... 
         with open('merged.json', 'w') as outfile:
             json.dump(output_file, outfile, indent=1)
         print("Merged!")
@@ -71,10 +71,10 @@ while True:
         with open(cwd) as json_file:
             data = json.load(json_file)
 
-        out = []
+        out = [] # same as in Merge, empty list to be extender
         for i in data:
-            if 'content' in i:
-                i["content"] = i["content"].encode('latin1').decode('utf8')
+            if 'content' in i: #checks if i has "content" key so it can decode it properly
+                i["content"] = i["content"].encode('latin1').decode('utf8') # facebook .json was encoded with utf-8 and wrongly decoded with latin-1, so I'm reversing this
                 # print(i)
                 out.append(i)
             if 'content' not in i:
@@ -89,8 +89,8 @@ while True:
         cwd = getcwd() + "/merged.json"
         file = open(cwd, "r", encoding="utf8")
         data = file.read()
-        counted_word_enc = counted_word.encode("unicode_escape").decode('utf8')
-        poss1 = data.count('"' + counted_word_enc + '"')
+        counted_word_enc = counted_word.encode("unicode_escape").decode('utf8') # to enable you to use characters like śż... etc
+        poss1 = data.count('"' + counted_word_enc + '"') # There is only 4 possibilities of word/sentence to appear in .json file(at least I think so)
         poss2 = data.count(' ' + counted_word_enc + '"')
         poss3 = data.count(' ' + counted_word_enc + ' ')
         poss4 = data.count('"' + counted_word_enc + ' ')
@@ -105,10 +105,10 @@ while True:
         with open(cwd) as json_file:
             data = json.load(json_file)
         for i in data:
-            if i["type"] == "Call":
+            if i["type"] == "Call": #checks if i type is call and adds call duration
                 a = i["call_duration"]
                 sum += a
-        days = (sum - sum % 86400) / 86400
+        days = (sum - sum % 86400) / 86400 # it is probably spaghetti code
         hours = (sum - days * 86400 - sum % 3600) / 3600
         minutes = (sum - days * 86400 - hours * 3600 - sum % 60) / 60
         seconds = (sum - days * 86400 - hours * 3600 - minutes * 60)
